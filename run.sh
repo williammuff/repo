b32c2907b1fbcb6a8d52af09a97dd14770819938
williammuff <william.muff@gmail.com>
2018-10-26 09:34:41 -0400

    Update to Repo

--- run.sh
+++ run.sh
@@ -2,3 +2,7 @@ rm -r Packages.bz2
 dpkg-scanpackages debs/ /dev/null > Packages
 bzip2 Packages
 #bzip2 -dk Packages.bz2
+echo USUAL PASSWORD no special characters
+git add .
+git commit -m "Update to Repo"
+git push origin master
