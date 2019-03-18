3a628dc7db440ddf7e60ab1a8b47695523edde7d
root <=>
2019-03-17 20:23:51 -0400

    Update To Repo

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 rm -r Packages.bz2
-dpkg-scanpackages debs/ /dev/null > Packages
+dpkg-scanpackages -m debs/ /dev/null > Packages
 bzip2 Packages
 #bzip2 -dk Packages.bz2
 echo USUAL PASSWORD no special characters
